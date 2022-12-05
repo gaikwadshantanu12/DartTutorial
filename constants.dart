c8aeb7c6f636aeb2972f20b9b6d9b94830c5d251
Shantanu Gaikwad <shantanu11agaikwad@gmail.com>
2022-12-05 16:10:51 +0530

    Constants in dart. Use of 'final' and 'const' keyword in dart.

---
+++ constants.dart
@@ -0,0 +1,23 @@
+void main() {
+  /*
+  - we can declare constants using 'const' and 'final' keywords
+
+  1. using const keyword
+      Syntax - const variable_name = variable_value;
+               const data_type variable_name = variable_value;
+
+  2. using final keyword
+      Syntax - final variable_name = variable_value;
+               final data_type variable_name = variable_value;
+  */
+
+  const name = "Shantanu";
+  const double PI = 3.14;
+  print(name);
+  print(PI);
+
+  final age = 21;
+  final int count = 0;
+  print(age);
+  print(count);
+}
